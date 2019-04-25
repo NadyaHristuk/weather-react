c17ed6adbecaa37f5f32bbdd61a4190dc7c4fcdf
Nadin <elhe@i.ua>
2019-04-25 17:05:59 +0300

    Updates

---
+++ precache-manifest.900ed3f5f9e762a607ab10b614a5a416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "15adb96a6ce6311faac9998db4f02f4e",
+    "url": "/weather-react/static/media/DefaultCityBG.15adb96a.jpg"
+  },
+  {
+    "revision": "f1050dd44b3c74947eba3be41121991d",
+    "url": "/weather-react/static/media/5c7d183b1217a_gettyimages_891953288.f1050dd4.jpeg"
+  },
+  {
+    "revision": "e22cd9d891db7975b1f2",
+    "url": "/weather-react/static/js/runtime~main.5429f5a8.js"
+  },
+  {
+    "revision": "70ecf49d0ae93ad5ab74",
+    "url": "/weather-react/static/js/main.ad405761.chunk.js"
+  },
+  {
+    "revision": "d95c13f4788c977b2200",
+    "url": "/weather-react/static/js/2.febe3071.chunk.js"
+  },
+  {
+    "revision": "70ecf49d0ae93ad5ab74",
+    "url": "/weather-react/static/css/main.42eada61.chunk.css"
+  },
+  {
+    "revision": "d95c13f4788c977b2200",
+    "url": "/weather-react/static/css/2.8f914cf6.chunk.css"
+  },
+  {
+    "revision": "0e3dd89a7b7cbc34cccf79a1e332bc47",
+    "url": "/weather-react/index.html"
+  }
+];
